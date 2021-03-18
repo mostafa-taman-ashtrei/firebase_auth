32c91579e0ab4425d37a2448ba8e8889d0ae0fbe
NEO <neo.one.mail@gmail.com>
2021-03-18 14:30:17 +0200

    add signup && login user

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     FormControl,
     FormLabel,
@@ -11,33 +11,55 @@ import {
     Link,
 } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
+import { auth } from '../firebase';
 
-const Login: React.FC = () => (
-    <Center>
-        <Box w="xl" p={8} shadow="2xl">
-            <Heading size="lg" mb="5">Login</Heading>
-            <form>
-                <FormControl id="email" isRequired>
-                    <FormLabel>Email</FormLabel>
-                    <Input type="email" size="lg" variant="filled" />
-                </FormControl>
+const Login: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
-                <FormControl id="password" isRequired>
-                    <FormLabel>Password</FormLabel>
-                    <Input type="password" size="lg" variant="filled" />
-                </FormControl>
+    const handleSubmit = async () => {
+        const user = await auth.signInWithEmailAndPassword(email, password);
+        console.log(user);
+    };
 
-                <Button background="green.400" mt="3" size="lg" w="full">Login</Button>
-            </form>
-            <Text mt="2.5">
-                Don
-                {'\''}
-                t have an account yet?
-                {' '}
-                <Link as={RouterLink} to="/register" color="teal.500">Signup</Link>
-            </Text>
-        </Box>
-    </Center>
-);
+    return (
+        <Center>
+            <Box w="xl" p={8} shadow="2xl">
+                <Heading size="lg" mb="5">Login</Heading>
+                <form>
+                    <FormControl id="email" isRequired>
+                        <FormLabel>Email</FormLabel>
+                        <Input
+                            type="email"
+                            size="lg"
+                            variant="filled"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormControl>
 
+                    <FormControl id="password" isRequired>
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                            type="password"
+                            size="lg"
+                            variant="filled"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormControl>
+
+                    <Button background="green.400" mt="3" size="lg" w="full" onClick={handleSubmit}>Login</Button>
+                </form>
+                <Text mt="2.5">
+                    Don
+                    {'\''}
+                    t have an account yet?
+                    {' '}
+                    <Link as={RouterLink} to="/register" color="teal.500">Signup</Link>
+                </Text>
+            </Box>
+        </Center>
+    );
+};
 export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     FormControl,
     FormLabel,
@@ -12,35 +12,81 @@ import {
 } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 
-const Register: React.FC = () => (
-    <Center>
-        <Box w="xl" p={8} shadow="2xl">
-            <Heading size="lg" mb="5">Sign up</Heading>
-            <form>
-                <FormControl id="email" isRequired>
-                    <FormLabel>Email</FormLabel>
-                    <Input type="email" size="lg" variant="filled" />
-                </FormControl>
+import { auth } from '../firebase';
 
-                <FormControl id="password" isRequired>
-                    <FormLabel>Password</FormLabel>
-                    <Input type="password" size="lg" variant="filled" />
-                </FormControl>
+const Register: React.FC = () => {
+    const [loading, setLoading] = useState<Boolean>(false);
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
 
-                <FormControl id="confirm-password" isRequired>
-                    <FormLabel>Confirm Password</FormLabel>
-                    <Input type="password" size="lg" variant="filled" />
-                </FormControl>
+    const handleSubmit = async () => {
+        if (password !== confirmPassword) {
+            alert('Passwords must match');
+            return;
+        }
+        try {
+            setLoading(true);
+            const res = await auth.createUserWithEmailAndPassword(email, password);
+            res.user?.sendEmailVerification();
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+        }
+        console.log(email, password, confirmPassword);
+        setLoading(false);
+    };
 
-                <Button background="green.400" mt="3" size="lg" w="full">Register</Button>
-            </form>
-            <Text mt="2.5">
-                Already have an account?
-                {' '}
-                <Link as={RouterLink} to="/login" color="teal.500">login</Link>
-            </Text>
-        </Box>
-    </Center>
-);
+    if (loading) return <h1>Loading</h1>;
+
+    return (
+        <Center>
+            <Box w="xl" p={8} shadow="2xl">
+                <Heading size="lg" mb="5">Sign up</Heading>
+                <form onSubmit={handleSubmit}>
+                    <FormControl id="email" isRequired>
+                        <FormLabel>Email</FormLabel>
+                        <Input
+                            type="email"
+                            size="lg"
+                            variant="filled"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormControl>
+
+                    <FormControl id="password" isRequired>
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                            type="password"
+                            size="lg"
+                            variant="filled"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormControl>
+
+                    <FormControl id="confirm-password" isRequired>
+                        <FormLabel>Confirm Password</FormLabel>
+                        <Input
+                            type="password"
+                            size="lg"
+                            variant="filled"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </FormControl>
+
+                    <Button background="green.400" mt="3" size="lg" w="full" onClick={handleSubmit}>Register</Button>
+                </form>
+                <Text mt="2.5">
+                    Already have an account?
+                    {' '}
+                    <Link as={RouterLink} to="/login" color="teal.500">login</Link>
+                </Text>
+            </Box>
+        </Center>
+    );
+};
 
 export default Register;
