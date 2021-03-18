0efa4da5a21684781b7fc5dc5c2a89fa23504126
NEO <neo.one.mail@gmail.com>
2021-03-18 10:23:46 +0200

    setup eslint && chakura ui

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+const Home: React.FC = () => (
+    <Box p={9} shadow="md" borderWidth="1px" m={8} textAlign="center">
+        <Heading fontSize="xl">Home page</Heading>
+    </Box>
+);
+
+export default Home;
